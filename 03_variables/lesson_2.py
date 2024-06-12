cc1ba1f308b651c87018d9a46e198a06a86aeab5
Kristina <kkanadore@gmail.com>
2024-06-12 20:11:02 +0300

    Task 2 Bus or metro

--- 03_variables/lesson_2.py
+++ 03_variables/lesson_2.py
@@ -3,3 +3,11 @@
 # a, b, c = 8, 5, 3
 # result = a / b + c - a / c
 # print(result)
+
+
+# Task 2 Bus or metro
+#
+# bus = 5
+# metro = 3
+# result = ((6 + (10 - bus) ** 2) / (metro + 1)) + (132 / (2 + bus))
+# print(result)
