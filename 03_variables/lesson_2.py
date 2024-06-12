0a96d0f9fb2af78addc211462ab0ab0cee8bace0
Kristina <kkanadore@gmail.com>
2024-06-12 20:18:53 +0300

    Task 3 Difficult powers

--- 03_variables/lesson_2.py
+++ 03_variables/lesson_2.py
@@ -11,3 +11,9 @@
 # metro = 3
 # result = ((6 + (10 - bus) ** 2) / (metro + 1)) + (132 / (2 + bus))
 # print(result)
+
+
+# Task 3 Difficult powers
+#
+# a = (-3 + (8 ** 2 - 12) * 4 ** (3 ** 2)) / (2 * 18)
+# print(a)
