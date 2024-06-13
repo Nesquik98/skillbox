182e24b5fb81f24284cf367c8b1f0b1cc4e320f9
Kristina <kkanadore@gmail.com>
2024-06-13 18:56:31 +0300

    Task 1 Apples

---
+++ 03_variables/lesson_4.py
@@ -0,0 +1,8 @@
+# Task 1
+#
+# apples = 41
+# boxes = 3
+# full_boxes = apples // boxes
+# print('Number of full boxes: ',full_boxes)
+# rest_apples = apples % boxes
+# print('Apples left: ',rest_apples)
