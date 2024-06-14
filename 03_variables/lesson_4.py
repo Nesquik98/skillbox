4a8b2da145bc2bd05954fdf746aa63add4fc7fe9
Kristina <kkanadore@gmail.com>
2024-06-14 18:51:46 +0300

    Task 2 Last digit

--- 03_variables/lesson_4.py
+++ 03_variables/lesson_4.py
@@ -1,4 +1,4 @@
-# Task 1
+# Task 1 Apples
 #
 # apples = 41
 # boxes = 3
@@ -6,3 +6,12 @@
 # print('Number of full boxes: ',full_boxes)
 # rest_apples = apples % boxes
 # print('Apples left: ',rest_apples)
+
+
+# Task 2 Last digit
+#
+# number = int(input('Enter the number: '))
+# number_home = number % 10
+# number_appart = number // 10
+# print('Nomber home: ', number_home)
+# print('Appartment namber: ', number_appart)
