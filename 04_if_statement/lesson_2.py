a09f3388161f58984e26df2b3891dc9b744c0e27
Kristina <kkanadore@gmail.com>
2024-06-22 20:58:30 +0300

    Task 1 Rate from Skillbox

---
+++ 04_if_statement/lesson_2.py
@@ -0,0 +1,9 @@
+# Task 1 Rate from Skillbox
+#
+# check = int(input('Сколько денег на счету?'))
+# if check < 75000:
+#     print('Не хватает денег на счету.')
+# else:
+#     print('Курс успешно приобретен.')
+# print('Хорошего дня!')
+
