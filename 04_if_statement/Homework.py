70d1fedd377c345db57daee8751161e4f248fbb4
Kristina <kkanadore@gmail.com>
2024-06-23 12:17:25 +0300

    Task 2 Admission

--- 04_if_statement/Homework.py
+++ 04_if_statement/Homework.py
@@ -5,3 +5,14 @@
 #     print('Пошёл дождь. Возьмите зонтик!')
 # else:
 #     print('Дождя нет.')
+
+
+# Task 2 Admission
+#
+# points_rus = int(input('Введите кол-во баллов по русскому языку: '))
+# points_math = int(input('Введите кол-во баллов по математике: '))
+# points_inform = int(input('Введите кол-во баллов по информатике: '))
+# if points_rus + points_math + points_inform >= 270:
+#     print('Поздравляю, ты поступил на бюджет!')
+# else:
+#     print('К сожалению, ты не прошел на бюджет.')
