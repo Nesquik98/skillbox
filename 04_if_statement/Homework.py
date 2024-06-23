ad62d4d413a133d2c16f9f57fcb5542ea93c7505
Kristina <kkanadore@gmail.com>
2024-06-23 12:09:31 +0300

    Task 1 Weather sensor

---
+++ 04_if_statement/Homework.py
@@ -0,0 +1,7 @@
+# Task 1 Weather sensor
+#
+# weather = int(input('На улице идет дождь? (1/0)'))
+# if weather == 1:
+#     print('Пошёл дождь. Возьмите зонтик!')
+# else:
+#     print('Дождя нет.')
