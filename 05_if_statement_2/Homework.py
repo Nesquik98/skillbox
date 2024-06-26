29388332944e84099c359e605710ef985b9b4a70
Kristina <kkanadore@gmail.com>
2024-06-26 21:57:28 +0300

    Task 1 Calculator of experience

---
+++ 05_if_statement_2/Homework.py
@@ -0,0 +1,11 @@
+# Test 1 Calculator of experience
+#
+# experience = int(input('Введите число очков опыта: '))
+# if experience < 1000:
+#     print('Ваш уровень: 1')
+# elif 1000 <= experience < 2500:
+#     print('Ваш уровень: 2')
+# elif 2500 <= experience < 5000:
+#     print('Ваш уровень: 3')
+# else:
+#     print('Ваш уровень: 4')
