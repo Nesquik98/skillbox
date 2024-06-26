20ad089c4ddb010c573dbbdb328f0b8286bee5fe
Kristina <kkanadore@gmail.com>
2024-06-26 15:13:29 +0300

    Task 1 Coordinates

---
+++ 05_if_statement_2/lesson_2.py
@@ -0,0 +1,10 @@
+# Task 1 Coordinates
+#
+# x = int(input('Введите координты икса: '))
+# y = int(input('Введите координаты игрека: '))
+# if x > y:
+#     print('x больше y')
+# elif x < y:
+#     print('x меньше y')
+# else:
+#     print('x равен y')
