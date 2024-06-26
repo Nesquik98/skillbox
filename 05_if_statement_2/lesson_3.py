782b32f6ca6d1084decd806bcd9f716304f7a071
Kristina <kkanadore@gmail.com>
2024-06-26 16:28:29 +0300

    Task 1 Buying a bike

---
+++ 05_if_statement_2/lesson_3.py
@@ -0,0 +1,8 @@
+# Task 1 Buying a bike
+#
+# speed_count = int(input('Введите количество скоростей: '))
+# year = int(input('Введите год выпуска: '))
+# if year < 2018 or speed_count < 24:
+#     print('Не соответствует критериям.')
+# else:
+#     print('Подходит.')
