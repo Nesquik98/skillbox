44d47a0a59d941329451580d4ccb048ab772fd39
Kristina <kkanadore@gmail.com>
2024-06-26 15:32:04 +0300

    Task 2 Progressive tax

--- 05_if_statement_2/lesson_2.py
+++ 05_if_statement_2/lesson_2.py
@@ -8,3 +8,20 @@
 #     print('x меньше y')
 # else:
 #     print('x равен y')
+
+
+# Task  2 Progressive tax
+#
+# wallet = int(input('Введите свою прибыль: '))
+# if wallet >= 0:
+#     if wallet < 10000:
+#         tax = wallet * 13 / 100
+#         print('Размер налога (13%) равен:', tax)
+#     elif wallet < 50000:
+#         tax = wallet * 20 / 100
+#         print('Размер налога (20%) равен:', tax)
+#     else:
+#         tax = wallet * 30 / 100
+#         print('Размер налога (30%) равен:', tax)
+# else:
+#     print('Ошибка: доход не может быть отрицательным.')
