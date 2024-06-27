2421a5f96d24c3fe893e9a736c90752cd4ee3d94
Kristina <kkanadore@gmail.com>
2024-06-27 11:53:40 +0300

    Task 11 Kostya wants to win

--- 05_if_statement_2/Homework.py
+++ 05_if_statement_2/Homework.py
@@ -132,3 +132,17 @@
 # else:
 #     print('Число не двузначное')
 
+
+# Task 11 Kostya wants to win
+#
+# num_1 = int(input('Первое число: '))
+# num_2 = int(input('Второе число: '))
+# num_3 = int(input('Третье число: '))
+# if num_1 == num_2 and num_1 == num_3:
+#     print('3')
+# elif num_1 == num_2 or num_1 == num_3 or num_2 == num_3:
+#     print('2')
+# else:
+#     print('0')
+
+
