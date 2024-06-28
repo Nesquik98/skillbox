df019220e471bfc2c2bd26a88f7b23bf8b0b6bdf
Kristina <kkanadore@gmail.com>
2024-06-28 19:38:10 +0300

    Task 2 Enter password

--- 06_while_loop/lesson_1.py
+++ 06_while_loop/lesson_1.py
@@ -6,3 +6,12 @@
 #     summ += number
 #     number = int(input('Введите число: '))
 # print(summ)
+
+
+# Task 2 Enter password
+#
+# password = int(input('Введите пароль: '))
+# while password != 235:
+#     print('Неверный пароль!')
+#     password = int(input('Попробуйте еще раз: '))
+# print('Пароль верный!')
