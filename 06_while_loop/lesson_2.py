3d1d0a4bc58b28a3805ef2f0bcb5d4395e738178
Kristina <kkanadore@gmail.com>
2024-06-30 18:40:40 +0300

    Task 1 Running is useful

---
+++ 06_while_loop/lesson_2.py
@@ -0,0 +1,13 @@
+# Task 1 Running is useful
+#
+# weather = int(input('Введите температуру: '))
+# run = 0
+# while weather > 15:
+#     run += 20
+#     weather -= 2
+#     if weather <= 15:
+#         print('Пройдено метров:', run)
+#         break
+#     run += 10
+#     print('Пройдено метров:', run)
+
