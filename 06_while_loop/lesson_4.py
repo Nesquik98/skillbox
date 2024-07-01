4eb82af613c7f9cf4fa36bc8eef55caba3351326
Kristina <kkanadore@gmail.com>
2024-07-01 10:59:57 +0300

    Task 3 Fishing

--- 06_while_loop/lesson_4.py
+++ 06_while_loop/lesson_4.py
@@ -14,3 +14,16 @@
 # while count < reminder:
 #     print('Вы хотели не забыть о чем-то')
 #     count += 1
+
+
+# Task 3 Fishing
+#
+# bags = int(input('Сколько нужно перетащить мешков?'))
+# count_weight = 0
+# count_bags = 0
+# while count_bags < bags:
+#     weight = int(input('Сколько весит мешок?'))
+#     count_weight += weight
+#     count_bags += 1
+#     print('Перенесли мешков', count_bags, 'из', bags)
+# print('Суммарный вес мешков:', count_weight)
