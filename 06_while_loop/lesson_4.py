a0f73a24410fb1d99e8c4798538dd5700cfebc19
Kristina <kkanadore@gmail.com>
2024-07-01 10:29:25 +0300

    Task 2 Reminder

--- 06_while_loop/lesson_4.py
+++ 06_while_loop/lesson_4.py
@@ -5,3 +5,12 @@
 # while count < num:
 #     print('Я - программист')
 #     count += 1
+
+
+# Task 2 Reminder
+#
+# reminder = int(input('Сколько раз напомнить?'))
+# count = 0
+# while count < reminder:
+#     print('Вы хотели не забыть о чем-то')
+#     count += 1
