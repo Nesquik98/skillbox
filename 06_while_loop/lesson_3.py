6f5647716d4bbc19528bd5876857bb0483f9813a
Kristina <kkanadore@gmail.com>
2024-07-01 09:15:04 +0300

    Task 2 Testing the application

--- 06_while_loop/lesson_3.py
+++ 06_while_loop/lesson_3.py
@@ -6,3 +6,13 @@
 #     count -= 1
 # print('Время вышло!')
 
+
+# Task 2 Testing the application
+#
+# while True:
+#     work = int(input('Продолжаем работать? 1/0: '))
+#     if work == 0:
+#         print('Приложение закрывается.')
+#         break
+# print('Работа завершена')
+
